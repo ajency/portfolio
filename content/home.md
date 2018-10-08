039f04aec1bfe449975efe9b77343c51922f7f48
Amit <amit@ajency.in>
2018-10-08 18:40:41 +0530

    css fixed

---
+++ content/home.md
@@ -0,0 +1,8 @@
+---
+title: "Ajency.in "
+date: 2018-06-04T12:57:20+05:30
+draft: false
+layout: home
+url: /home/
+
+---
---
+++ themes/portfolio/static/js/custom.js
@@ -0,0 +1,31 @@
+
+
+
+
+jQuery('.variable-width').slick({
+ dots: false,
+      arrows:false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+    
