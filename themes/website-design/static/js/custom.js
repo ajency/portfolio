7f51f9e261aefdd77c4cbb14deb5655b60c3315b
Amit <amit@ajency.in>
2018-10-11 20:04:42 +0530

    css fixed

--- themes/website-design/static/js/custom.js
+++ themes/website-design/static/js/custom.js
@@ -49,3 +49,14 @@ $('.aj-team-member').slick({
   slidesToShow: 1,
   variableWidth: true
 });
+
+
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+    }
+});
